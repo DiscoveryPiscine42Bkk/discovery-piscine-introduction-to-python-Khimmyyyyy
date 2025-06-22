96310bf7b8f71e9556cfb3f75e685bdb1bbaef36
Khimmyyyyy <kimmykhim02@gmail.com>
2025-06-22 05:40:47 +0000

    add file

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = input()
+
+if password == "Python is awsome":
+    print ("ACCESS GRANTED")
+else:
+    print ("ACCESS DENIED")   
---
+++ cell03/ex00/to25.py
@@ -0,0 +1,14 @@
+def main():
+    try: 
+        num = int(input("Enter a number less than 25: "))
+        if num > 25:
+            print("Error")
+            return 
+        while num <= 25:
+            print(f"Inside the loop, my variable is {num}")
+            num += 1 
+    except ValueError:
+        print("Please enter a valid number.") 
+
+if __name__=="__main__":
+    main()             
